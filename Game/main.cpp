ffd773e264d92bffb79de9d9f572290db11a99a8
Quintin <qcrist@gmail.com>
2013-01-17 08:18:12 -0500

    Added basic outline of 3d application

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,6 +1,42 @@
 #include "main.h"
 
-int main()
+int main(int argc, char** args)
 {
+	//GLUT
+	glutInit(&argc, args);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(800,600);
+	glutCreateWindow("OpenGL Window");
+	glutDisplayFunc(draw);
+	glutIdleFunc(draw);
+	glutReshapeFunc(resize);
+
+	//GLEW
+	glewInit();
+
+	//OpenGL
+	glClearColor(.1f,.1f,.1f,1);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+	glPolygonMode(GL_BACK,GL_LINE);
+
+	//Start
+	glutMainLoop();
 	return 0;
+}
+
+void resize(int w, int h)
+{
+	if (h == 0)h = 1;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluPerspective(45,(float)w/h,.1,1000);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
 }
